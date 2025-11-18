b129dd31b644a778118d611177d291f8bb7774f0
B-K-Bala <123554732+B-K-Bala@users.noreply.github.com>
2025-11-18 10:53:27 +0530

    Add files via upload

---
+++ Problem-1.py
@@ -0,0 +1,39 @@
+from matplotlib.pylab import double
+
+
+class clacultor:
+    def __init__(self,a,b):
+        self.a =a
+
+        self.b=b
+
+    def add(self):
+        return self.a + self.b
+    def sub(self):
+        return self.a - self.b  
+    def mul(self):
+        return self.a * self.b
+    def div(self):
+        if self.b==0:
+            return f"Cannot Divide the {self.a} by zero"
+        return self.a / self.b
+    
+if __name__ == "__main__":
+    a=double(input("Enter first number: "))
+    b=double(input("Enter second number: "))
+    operation=input("Enter operation (add,sub,mul,div): ")
+
+    cal = clacultor(a,b)
+    if operation == "add":
+        print(f"Addition {a} {operation} {b} :",cal.add())
+    elif operation == "sub":
+        print(f"Subtraction {a} {operation} {b} :",cal.sub())
+    elif operation == "mul":
+        print(f"Multiplication {a} {operation} {b} :",cal.mul())
+    elif operation == "div":
+        print(f"Division {a} {operation} {b} :",cal.div())
+    else:
+        print("Unknown operation")
+        
+     
+        
